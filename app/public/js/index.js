1a4739de9d8b261598fee12248e90e8d1c15ba36
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-11-19 03:31:45 -0600

    actualizado (integracion mapa a aplicacion)

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -122,3 +122,13 @@ document.getElementById('nextMonth').addEventListener('click', () => {
 
 updateMonthYearDisplay();
 generateCalendar(); 
+
+var element = document.getElementById('map');
+var map = L.map(element);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+var target = L.latLng('47.50737', '19.04611');
+map.setView(target, 14);
+L.marker(target).addTo(map);
